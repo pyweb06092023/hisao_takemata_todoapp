304d07d5ab3a1bc7a6b5fd52c39f74cfb30d0891
pyweb06092023 <pywebpyweb024@gmail.com>
2026-08-25 11:21:45 +0900

    20260825-Chapter13-登録画面の生成による作成

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -1,15 +1,49 @@
 package com.example.todoapp;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    private final TodoMapper todoMapper;
+
+    public HomeController(TodoMapper todoMapper) {
+        this.todoMapper = todoMapper;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "やること管理");
         return "index";
+    }
+
+    @GetMapping("/todos")
+    public String todos(Model model) {
+        List<Todo> todos = todoMapper.findAll();
+        model.addAttribute("todos", todos);
+        return "todos";
+    }
+
+    @GetMapping("/todos/new")
+    public String createForm(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "create";
+    }
+
+    @PostMapping("/todos/confirm")
+    public String createConfirm(@ModelAttribute Todo todo, Model model) {
+        model.addAttribute("todo", todo);
+        return "create-confirm";
+    }
+
+    @PostMapping("/todos/new")
+    public String createFormBack(@ModelAttribute Todo todo, Model model) {
+        model.addAttribute("todo", todo);
+        return "create";
     }
 }
