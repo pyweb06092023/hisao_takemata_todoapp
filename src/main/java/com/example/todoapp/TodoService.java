3e4a09216762d329548801d23f71aa01fbde0d94
pyweb06092023 <pywebpyweb024@gmail.com>
2026-08-25 15:24:29 +0900

    20260825-Chap20-Log出力（日本語化-未対応

--- src/main/java/com/example/todoapp/TodoService.java
+++ src/main/java/com/example/todoapp/TodoService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class TodoService {
     private final TodoMapper todoMapper;
 
@@ -22,13 +25,16 @@ public class TodoService {
 
     public void create(Todo todo) {
         todoMapper.insert(todo);
+        log.info("Todo created. id={}", todo.getId());
     }
 
     public void update(Todo todo) {
         todoMapper.update(todo);
+        log.info("Todo updated. id={}", todo.getId());
     }
 
     public void delete(Long id) {
         todoMapper.deleteById(id);
+        log.info("Todo deleted. id={}", id);
     }
 }
